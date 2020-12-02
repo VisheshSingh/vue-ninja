373547b0a9e0617a526d221b6e1a0797e919f442
Vishesh Singh <VisheshSingh16@gmail.com>
2020-12-01 18:08:09 -0600

    Methods & Click Events

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const app = Vue.createApp({
   data() {
     return {
-      title: 'The Final Empire',
+      title: 'Final Empire',
       author: 'Brandon Sanderson',
+      age: 45,
     };
+  },
+  methods: {
+    changeTitle(title) {
+      this.title = title;
+    },
   },
 });
 
