256f5fbfb8e154fab4726f815dbc4111aeedde49
Vishesh Singh <VisheshSingh16@gmail.com>
2020-12-01 18:44:38 -0600

    attribute-binding + dynamic classes

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ const app = Vue.createApp({
   data() {
     return {
       showData: true,
+      url: 'http://www.thenetninja.co.uk',
       books: [
-        { title: 'The Final Empire', author: 'Brandon Sanderson' },
-        { title: 'The Way of Kings', author: 'Patrick Rofus' },
-        { title: 'Harry Potter', author: 'J.K Rowling' },
+        {
+          title: 'The Name of wind',
+          author: 'Patrick Rofus',
+          img: 'assets/1.jpg',
+          isFav: true,
+        },
+        {
+          title: 'The Way of Kings',
+          author: 'B. Sanders',
+          img: 'assets/2.jpg',
+          isFav: false,
+        },
+        {
+          title: 'The Final Empire',
+          author: 'Brandon Sanderson',
+          img: 'assets/3.jpg',
+          isFav: true,
+        },
       ],
     };
   },
